bfca2e6fec9ce4a07aafba820aeebae3b6442854
Richie <richie@Richies-MacBook-Pro.local>
2022-03-23 17:25:51 -0800

    SDAS

--- src/components/Pages/amazonPriceTracker/login/SignUp.js
+++ src/components/Pages/amazonPriceTracker/login/SignUp.js
@@ -21,6 +21,7 @@ function SignUp() {
 
     if (matchedPassWord === newPassword) {
       fetch(`https://richiehayden-portfolio-backend.herokuapp.com/newuser`, {
+        credentials: "include",
         method: "post",
         headers: {
           "Content-Type": "application/json",
