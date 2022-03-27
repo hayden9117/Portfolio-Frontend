d4e6795559fc63984dbb0daeb8b158d4ed5976f4
Richie <richie@Richies-MacBook-Pro.local>
2022-03-26 18:24:26 -0800

    more responsive, added drawer to nav for mobile

--- src/components/Pages/amazonPriceTracker/PriceNavBar/PriceTrackerNav.js
+++ src/components/Pages/amazonPriceTracker/PriceNavBar/PriceTrackerNav.js
@@ -31,14 +31,7 @@ function PriceTrackerNav() {
     setMode("dark");
   }
   return (
-    <Box
-      sx={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        textAlign: "center",
-      }}
-    >
+    <>
       <CssBaseline />
       <AppBar sx={{ height: "15", position: "fixed" }}>
         <Container maxWidth="xl">
@@ -71,42 +64,7 @@ function PriceTrackerNav() {
           </Toolbar>
         </Container>
       </AppBar>
-    </Box>
-
-    // <Box
-    //   sx={{
-    //     bgcolor: "background.paper",
-    //     display: "flex",
-    //     height: "100%",
-    //     width: "100%",
-    //     textAlign: "center",
-    //   }}
-    // >
-    //   <CssBaseline />
-    //   <AppBar>
-    //     <Toolbar>
-    //       <ProductInput />
-    //       <Box
-    //         sx={{ position: "fixed", right: "0%", display: "flex" }}
-    //         justifyContent="start"
-    //       >
-    //         <Typography
-    //           sx={{
-    //             " @media screen and (max-width: 650px)": {
-    //               display: "none",
-    //             },
-    //           }}
-    //           variant="h5"
-    //         >{`Welcome, ${token.token.username}`}</Typography>
-    //         <AccountMenu />
-    //         <Button color={"inherit"} onClick={toggleChecked}>
-    //           {" "}
-    //           {checked === true ? <LightModeIcon /> : <DarkModeIcon />}
-    //         </Button>
-    //       </Box>
-    //     </Toolbar>
-    //   </AppBar>
-    // </Box>
+    </>
   );
 }
 export default PriceTrackerNav;
